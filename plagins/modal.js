29ec30df116fa726cc2a2e4178d7c466f1889fc8
Хворых Виталий <vithvo@bk.ru>
2022-07-11 18:13:13 +0700

    added JS files

--- plagins/modal.js
+++ plagins/modal.js
@@ -1,1 +1,38 @@
-$.modal = function (param) {};
+function _createModal(options) {
+  const modal = document.createElement("div");
+  modal.classList.add("vmodal");
+  modal.insertAdjacentHTML(
+    "afterbegin",
+    `
+      <div class="modal__overlay">
+        <div class="modal__window">
+          <div class="modal__header">
+            <span class="modal__title">Modal title</span>
+            <span class="modal__close">&times;</span>
+          </div>
+          <div class="modal__content">
+            <p>Lorem ipsum dolor sit.</p>
+            <p>Lorem ipsum dolor sit.</p>
+          </div>
+          <div class="modal__footer">
+            <button>OK</button>
+            <button>CANCEL</button>
+          </div>
+        </div>
+      </div>
+   `
+  );
+
+  document.body.appendChild(modal);
+  return modal;
+}
+
+$.modal = function (options) {
+  const $modal = _createModal(options);
+
+  return {
+    open() {},
+    close() {},
+    destroy() {},
+  };
+};
